f17940bf00acd9ccdf0c4917352e985eb5e3a6ad
mariam-bel <merimarabel@gmail.com>
2025-04-07 23:54:13 +0200

    Update simpson.py

--- simpson.py
+++ simpson.py
@@ -1,4 +1,3 @@
-from random import random
 import requests
 import random as r
 url = 'https://thesimpsonsquoteapi.glitch.me/quotes'
@@ -6,10 +5,10 @@ webContent = requests.get(f"{url}")
 personaje = webContent.json()
 valores = list(personaje[0].values())
 cita = valores[0]
-nombre = valores[1].lower()
+nombre = valores[1]
 print(nombre)
 
-def llenarNombre(nombre):
+def rellenarNombre(nombre):
     palabra = ""
     for i in range(len(nombre)):
         if nombre[i] == " ":
@@ -18,43 +17,45 @@ def llenarNombre(nombre):
             palabra += "_"
     return palabra
 
-def AgregarLetras(nombre):
-    letra = ""
+def letras(nombre):
+    letra = []
     pos = 0
-    for i in range(3):
-        pos = random.randint(0,len(nombre))
-        letra = nombre[pos]
+    for i in range(int(len(nombre)/4)):
+        pos = r.randint(0,len(nombre)-1)
+        letra.append(nombre[pos])
+    return letra
 
-barrasbajas = llenarNombre(nombre)
-cont = 0
+def nuevoNombre(nombre, letras):
+    palabra = ""
+    for i in range(len(nombre)):
+        if nombre[i] == " ":
+            palabra += " "
+        elif nombre[i] in letras:
+            palabra += nombre[i]
+        else:
+            palabra += "_"
+    return palabra
+
 continuar = True
 print(cita)
-long = list("")
-for i in range(len(nombre)):
-    if nombre[i]==" ":
-        long+=nombre[i]
-    else:
-        long += "_"
-
 cont = 0
-letras = len(nombre)/3
-
+nint = 1
+print(rellenarNombre(nombre))
+letras_viejas = []
 while continuar:
-    print(str(long))
-    print(llenarNombre(nombre))
-    intento = input("Adivina el nombre, ¡tienes 3 intentos! : ")
-    if intento == nombre:
-        print("Enhorabuena paquete...")
+    print("Adivina el autor de la cita ")
+    intento = input(f"Intento {nint}: ").lower().capitalize()
+    if intento == nombre.lower().capitalize():
+        print("¡Enhorabuena!")
         continuar = False
     elif cont<2:
-        for i in range(int(letras)):
-            letra = r.randint(0,len(nombre)-1)
-            while long[letra]!= "_":
-                letra = r.randint(0,len(nombre)-1)
-            long[letra]=nombre[letra]
-        cont+=1
-    elif cont>=2:
-        print(f"El personaje es: {nombre}")
+        letrasNombre = letras(nombre)
+        for letra in letrasNombre:
+            if letra not in letras_viejas:
+                letras_viejas.append(letra)
+        print(nuevoNombre(nombre, letras_viejas))
+        cont += 1
+    else:
+        print(f"Has perdido... El personaje es: {nombre}")
         continuar = False
-
-
+    nint+=1
